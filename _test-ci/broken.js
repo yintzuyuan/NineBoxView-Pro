786e586b0c62c50d2f59e0592cffbb11b8b8eb51
Erikyin <83154914+yintzuyuan@users.noreply.github.com>
2026-01-19 03:53:00 +0800

    test: 加入故意錯誤檔案測試 CI workflow

---
+++ _test-ci/broken.js
@@ -0,0 +1,11 @@
+// CI Test - JavaScript with undefined variables
+
+// 故意的錯誤：使用未定義的變數
+console.log(undefinedVariable);
+
+// 故意的錯誤：呼叫不存在的函數
+nonExistentFunction();
+
+// 正常的程式碼
+const validVar = "This is valid";
+console.log(validVar);
